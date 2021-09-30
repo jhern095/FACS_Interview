56b8f334fd3489fd147b4fb4ac3d697c0e3c1e2d
jhern095 <jeremyhernandez382@gmail.com>
2021-09-30 11:08:29 -0700

    New Automation Project

---
+++ PersonalAutomationProject/TestAutomation/Selenium.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Selenium.WebDriver;
+using Selenium.Helper;
+using Selenium.Internal.SeleniumEmulation;
+using SeleniumExtras.PageObjects;
+using Selenium.WebDriver.WaitExtensions;
+using Selenium.WebDriver.Extensions;
+using Selenium.WebDriver.WaitExtensions.WaitTypeSelections;
+using Selenium.WebDriver.WaitExtensions.WaitConditions;
+
+namespace PersonalAutomationProject
+{
+    class TestAutomationSampe
+    {
+    }
+}
